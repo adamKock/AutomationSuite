923445507c73c30ef6607213c6f87fb8a0b2ec85
AdamKockelbergh <adamkockelbergh@gmail.com>
2025-06-06 10:18:58 +0100

    logintest

--- src/main/java/com/swaglabs/uipages/loginPage.java
+++ src/main/java/com/swaglabs/uipages/loginPage.java
@@ -12,6 +12,7 @@ public class loginPage {
     static String url = "https://www.saucedemo.*"; 
 
     //write login method here
+    //test commit
 
     public void login(Page page){
         page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("username")).fill(username);
