81c517755721ba811eec0ed19b8d96d37c5a7d28
AdamKockelbergh <adamkockelbergh@gmail.com>
2025-06-06 11:15:32 +0100

    Update browser headlessmode

--- src/main/java/com/swaglabs/BrowserUtil/browserUtil.java
+++ src/main/java/com/swaglabs/BrowserUtil/browserUtil.java
@@ -14,7 +14,7 @@ public class browserUtil {
 
     public browserUtil() {
         this.playwright = Playwright.create();
-        this.browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        this.browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
         this.context = browser.newContext();
     }
 
